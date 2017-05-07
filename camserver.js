94e86804dabab7cc388e49dbce037caf2a43f941
Sai Anusha Mulumoodi <mulumoodi@Sais-MacBook-Pro.local>
2017-05-06 18:20:07 -0700

    Carousel UI

--- camserver.js
+++ camserver.js
@@ -2,16 +2,16 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var watson = require('watson-developer-cloud');
-var client = require('./connection.js'); 
-var moment = require('moment'); 
+var client = require('./connection.js');
+var moment = require('moment');
 
 var current_date = moment().format("YYYY-MM-DD");
 var current_time = moment().format("h:mm:ss");
 
-var temp = require('temp-sdk');
-temp.config.update({accessKeyId:"",
-secretAccessKey:"",
-"region":"us-west-2"});
+var AWS = require('aws-sdk');
+AWS.config.update({accessKeyId:"AKIA 1234 JYFGM 5678  QSSIVNZV 9101112 2DA",
+ secretAccessKey:"lR37 1234 TyiZIIv 5678 9101112 sXcMnUogf4T/vjr 13141516 GE4yxly8KnzFYS",
+ "region":"us-west-2"});;
 
 var visual_recognition = watson.visual_recognition({
   api_key: 'c23b8c3f8eb01911834d2c50444f63cea40d56d2',
@@ -20,7 +20,7 @@ var visual_recognition = watson.visual_recognition({
 });
 
 
-var sns = new temp.SNS();
+var sns = new AWS.SNS();
 var sendNotif=function(object){
     var params = {
     Message: 'STRING_VALUE', /* required */
@@ -32,8 +32,8 @@ var sendNotif=function(object){
       /* '<String>': ... */
     },
     MessageStructure: 'json',
-    Message:'{"default":"Some more shit","email":"Something unusual is happening"}',
-    Subject: 'Be alert!!',
+    Message:'{"default":"Some more shit","email":"Something unusual is happening'+object+'"}',
+    Subject: 'SmartCams - Be alert!!',
     //TargetArn: 'arn:aws:sns:us-west-2:394476956221:alertQueue',
     TopicArn: 'arn:aws:sns:us-west-2:394476956221:alertQueue'
     };
@@ -70,30 +70,36 @@ if (page == '/'){
             console.log("Waiting a sec");
             setTimeout(function(){
               var params = {
-                images_file: fs.createReadStream('/Users/ambikabohra/Downloads/dl.png')
+                images_file: fs.createReadStream('/Users/mulumoodi/Downloads/dl.png')
               };
 
-              visual_recognition.classify(params, function(err, res) {
-                if (err){
-                  console.log("Somethings wrong with the service");
+              visual_recognition.detectFaces(params, function(err, res) {
+                if (err)
                   console.log(err);
-                }
                 else{
-                  var jstring=JSON.stringify(res, null, 2);
-                  console.log(jstring);
-                  if(jstring.indexOf("thumb") > -1){
-                      console.log("\n\nThumb is there!!!\n\n")
+                  console.log(JSON.stringify(res, null, 2));
+                  var jstring=res;//.stringify(res, null, 2);
+                  console.log(jstring["images"]);
+                    if(jstring["images"][0]["faces"].length){
+                      var human_count_val=jstring["images"][0]["faces"].length;
+                      console.log(jstring["images"][0]["faces"].length+"ppl found")
+                      var obj4=jstring["images"][0]["faces"][0];
+                      var obj5=jstring["images"][0]["faces"][0]["age"]["min"];
+                      var obj6=jstring["images"][0]["faces"][0]["age"]["max"];
+                      var obj7=jstring["images"][0]["faces"][0]["gender"]["gender"];
+                      console.log(obj5);
+                      console.log(obj6);
+                      console.log(obj7);
+                      console.log("A "+obj7+" of age "+obj5+"-"+obj6+" found");
 
-                  }
-		  if((jstring.indexOf("person") > -1) || (jstring.indexOf("people") > -1)){
-                      console.log("\nAlert, Someone is in secure room !!!!! \n\n")
-                      sendNotif("dangeralert");
-                      client.index({   
-                        index: 'image_index',
+                      sendNotif("A "+obj7+" of age "+obj5+"-"+obj6+" found");
+                      client.index({
+                        index: 'demographic_index',
                         type: 'datetime_score',
                         body: {
                           "current_Time":current_time,
                           "current_Date":current_date,
+                          "human_count":human_count_val,
                           "scores":res
                         }
                       },function(err,resp,status) {
@@ -103,13 +109,14 @@ if (page == '/'){
                         else {
                           console.log("create",resp);
                         }
-                      }) 
-                  }
+                      })
 
-
-                  fs.unlinkSync('/Users/ambikabohra/Downloads/dl.png');
-                  console.log("Deletion succesful");
+                    }
+                    fs.unlinkSync('/Users/mulumoodi/Downloads/dl.png');
+                    console.log("Deletion succesful");
                 }
+
+
               });
 
             }, 1000);
@@ -120,4 +127,3 @@ if (page == '/'){
       }
 });
 server.listen(8080);
-
